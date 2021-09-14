06faa5c1333806bbc71fb6f852489152119a670c
a7ridi <a7ridi@github.com>
2021-09-14 12:48:08 +0530

    airbnb clone

---
+++ pages/home/_id.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <div style="display: flex">
+      <img
+        v-for="image in home.images"
+        :key="image"
+        :src="image"
+        width="200"
+        height="150"
+      />
+    </div>
+    {{ home.title }} <br />
+    ${{ home.pricePerNight }} / night <br />
+    <img src="/images/marker.svg" alt="marker" width="20" height="20" />
+    {{ home.location.address }} {{ home.location.city }}
+    {{ home.location.state }} {{ home.location.country }} <br />
+    <img src="/images/star.svg" alt="star" width="20" height="20" />{{
+      home.reviewValue
+    }}
+    <br />
+    {{ home.guests }} guests, {{ home.bedroom }} rooms, {{ home.beds }} beds,
+    {{ home.bathrooms }} bath <br />
+    <div style="height: 800px; width: 800px" ref="map"></div>
+  </div>
+</template>
+
+<script>
+import homes from '~/data/homes'
+// if (process.client) {
+//   window.initMap = function () {
+//     console.log('test')
+//   }
+// }
+// console.log(process.client)
+export default {
+  components: {
+    homes,
+  },
+  head() {
+    return {
+      title: this.home.title,
+      // script: [
+      //   {
+      //     src: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCLXZSSD1S9aK864l_k4ekcz3AHoCIwTZU&libraries=places&callback=initMap',
+      //     hid: 'map',
+      //     async: true,
+      //     skip: process.client && window.mapLoaded,
+      //   },
+      //   {
+      //     innerHTML: 'window.initMap = function() { window.mapLoaded = true }',
+      //     hid: 'map-init',
+      //   },
+      // ],
+      // __dangerouslyDisableSanitizersByTagID: {
+      //   'map-init': ['innerHTML'],
+      // },
+    }
+  },
+  data() {
+    return {
+      home: [],
+    }
+  },
+  methods: {
+    // showMap() {
+    //   const mapOptions = {
+    //     zoom: 18,
+    //     center: new window.google.maps.LatLng(
+    //       this.home._geoloc.lat,
+    //       this.home._geoloc.lng
+    //     ),
+    //     disableDefaultUI: true,
+    //     zoomControl: true,
+    //   }
+    //   const map = new window.google.maps.Map(this.$refs.map, mapOptions)
+    //   const position = new window.google.maps.LatLng(
+    //     this.home._geoloc.lat,
+    //     this.home._geoloc.lng
+    //   )
+    //   const marker = new window.google.maps.Marker({ position })
+    //   marker.setMap(map)
+    // },
+  },
+  mounted() {
+    // const timer = setInterval(() => {
+    //   if (window.mapLoaded) {
+    //     clearInterval(timer)
+    //     this.showMap()
+    //   }
+    // })
+    this.$maps.showMap(
+      this.$refs.map,
+      this.home._geoloc.lat,
+      this.home._geoloc.lng
+    )
+  },
+  created() {
+    const home = homes.find((home) => home.objectID === this.$route.params.id)
+    this.home = home
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div
+      :key="home.objectID"
+      v-for="home in homes"
+      style="float: left; margin: 10px"
+    >
+      <nuxt-link :to="`/home/${home.objectID}`">
+        <HomeCard :home="home"
+      /></nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import homes from '~/data/homes'
+
+export default {
+  head() {
+    return {
+      title: 'Homepage',
+      meta: [
+        {
+          name: 'description',
+          content: 'This is a homepage',
+          hid: 'description',
+        },
+      ],
+    }
+  },
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    }
+  },
+}
+</script>
